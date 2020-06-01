7677c8f68c1dd3cb9f17a44d55a606ee565b944f
tomii1031 <57342235+tomii1031@users.noreply.github.com>
2020-06-01 15:36:30 +0900

    create login register vue

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,101 @@
-@extends('layouts.template')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
 
-@section('content')
- <div class="container">
-    <div class="row justify-content-center">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://unpkg.com/ress/dist/ress.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+
+
+
+    <!-- Styles -->
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+
+
+<body>
+    <div class="login vue" style="background-image: url({{ asset('images/login-back9.jpg') }});  background-size: cover;">
+     <header class="page-header">
+        <nav>
+            <ul class="main-nav">
+                <li class="app-name menu h4 ml-3"><a href="{{ url('/') }}">MyLibrary</a></li>
+                <li class="menu ml-5"><a href="index">書籍の一覧</a></li>
+                <li class="menu ml-5"><a href="create">書籍の入力</a></li>
+                <li class="menu ml-5"><a href="#">お問い合わせ</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="home-content wrapper">
+        <h2 class="page-title">
+            There is no meaning unless output
+        </h2>
+        <p class="sub-title">
+            本を読んだら、すぐにアウトプット！知識の定着をサポート!
+        </p>
+    </div>
+    <div class="login-form">
+        <form method="POST" class="mt-3" action="{{ route('login') }}">
+            @csrf
+        <div class="form-mail">
+        <h2 class="login-title">{{ __('Login') }}</h2>
+        <label class="mr-3 mt-2" for="email">{{ __('メールアドレス') }}</label>
+        <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}">
+        @error('email')
+            <span class="invalid-feedback" role="alert">
+                 <strong>{{ $message }}</strong>
+                </span>
+        @enderror
+        </div>
+    <div class="form-password">
+        <label for="password" class="mr-5 my-2">{{ __('パスワード') }}</label>
+        <input id="password" type="password" class="@error('password') is-invalid @enderror" name="password" >
+        @error('password')
+          <span class="invalid-feedback" role="alert">
+             <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+    </div>
+    <div class="form-check">
+        {{-- <input class="form-check-input checkbox" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}> --}}
+
+        {{-- <label class="form-check-label" for="remember">
+            {{ __('Remember Me') }}
+        </label> --}}
+        <div class="login-btn">
+                <button type="submit" class="btn btn-success">
+                    {{ __('ログイン') }}
+                </button>
+
+                @if (Route::has('password.request'))
+                <p><a class="btn btn-link mt-3" href="{{ route('password.request') }}">
+                     {{ __('パスワードを忘れた方はこちら') }}
+                    </a></p>
+                @endif
+    </div>
+</div>
+</form>
+
+     {{-- <div class="container">
+
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header bg-warning h3">{{ __('Login') }}</div>
+
+                <div class="card-header bg-warning h3">{{ __('Login') }}</>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -23,7 +113,7 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
@@ -37,7 +127,7 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+
 
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
@@ -70,4 +160,10 @@
         </div>
     </div>
 </div>
-@endsection
+</body>
+ --}}
+</div>
+
+
+ </body>
+ </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,113 @@
-@extends('layouts.template')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
 
-@section('content')
-<div class="container">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://unpkg.com/ress/dist/ress.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+
+
+
+    <!-- Styles -->
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+</head>
+
+<body>
+    <div class="login vue" style="background-image: url({{ asset('images/login-back9.jpg') }});  background-size: cover;">
+    <header class="page-header">
+        <nav>
+            <ul class="main-nav">
+                <li class="app-name menu h4 ml-3"><a href="{{ url('/') }}">MyLibrary</a></li>
+                <li class="menu ml-5"><a href="index">書籍の一覧</a></li>
+                <li class="menu ml-5"><a href="create">書籍の入力</a></li>
+                <li class="menu ml-5"><a href="#">お問い合わせ</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+    <div class="home-content wrapper">
+        <h2 class="page-title">
+            There is no meaning unless output
+        </h2>
+        <p class="sub-title">
+            本を読んだら、すぐにアウトプット！知識の定着をサポート!
+        </p>
+    </div>
+
+    <div class="register-form">
+        <div class="register-title">{{ __('新規登録') }}
+        </div>
+        <form method="POST" class="mt-3" action="{{ route('register') }}">
+            @csrf
+
+
+        <div class="form-name">
+         <label for="name" class="mr-4 my-3">{{ __('ニックネーム') }}</label>
+         <input id="name" type="text" class="@error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+         @error('name')
+          <span class="invalid-feedback" role="alert">
+             <strong>{{ $message }}</strong>
+          </span>
+         @enderror
+        </div>
+
+
+        <div class="form-email">
+            <label for="email" class="mr-3 my-3">{{ __('メールアドレス') }}</label>
+            <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+            @error('name')
+            <span class="invalid-feedback" role="alert">
+             <strong>{{ $message }}</strong>
+            </span>
+               @enderror
+        </div>
+
+
+        <div class="form-password">
+            <label for="password" class="mr-5 my-3">{{ __('パスワード') }}</label>
+            <input id="password" type="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+            @error('password')
+              <span class="invalid-feedback" role="alert">
+               <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+            </div>
+
+        <div class="form-password-confirm">
+            <label for="password-confirm" class="mr-5 my-3">{{ __('パスワード') }}</label>
+            <input id="password-confirm" type="password"  name="password_confirmation" required autocomplete="new-password">
+        </div>
+
+        <div class="register-btn">
+            <button type="submit" class="btn btn-primary">
+                {{ __('新規登録する') }}
+            </button>
+        </div>
+
+    </div>
+</form>
+
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header h3 bg-success">{{ __('Register') }}</div>
+                <div class="card-header h3 bg-success">{{ __('新規登録') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -73,5 +175,7 @@
             </div>
         </div>
     </div>
+</div> --}}
 </div>
-@endsection
+</body>
+</html>
